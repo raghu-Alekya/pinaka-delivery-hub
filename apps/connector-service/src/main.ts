2e23ccb6ca2f15c3da5e37b30d729a2426d54314
raghu-Alekya <Raghu.Kammary@AlekyaTechSolutions.com>
2026-08-05 15:20:05 +0530

    Added reflect-metadata Import: Added import 'reflect-metadata'; to the very top line of apps/order-service/src/main.ts  and apps/connector-service/src/main.ts Registered Global Validation Pipe: Registered app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true })); in both microservices.

--- apps/connector-service/src/main.ts
+++ apps/connector-service/src/main.ts
@@ -1,8 +1,11 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`🚀 Connector Service running on http://localhost:${port}`);
